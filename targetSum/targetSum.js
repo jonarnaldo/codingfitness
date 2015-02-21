8a50bc83d92e09ec985cfc97fc9714920fd8e35b
jonarnaldo <jonathanarnaldo@Jonathans-MacBook-Air.local>
2015-02-21 12:49:12 -0800

    adds targetSum

--- targetSum/targetSum.js
+++ targetSum/targetSum.js
@@ -8,6 +8,38 @@
 // If there are two pairs of integers with the same sum, any pair can be returned.
 // If the length of the original unsorted array is less than 2, return null.
 
+
 var targetSum = function(array, n){
   // Your code here
+  var res = [];
+  var compare = 0;
+  var i = 0;
+  var j = i + 1;
+
+  if (array.length < 2) {
+    return null;
+  }
+
+  while (i < array.length && j < array.length) {
+    var sum = (array[i] + array[j]);
+    console.log(sum);
+    if (sum <= n && sum > compare) {
+      res.pop();
+      res.pop();
+      res.push(array[i]);
+      res.push(array[j]);
+      compare = sum; 
+    };
+    j++;
+
+    if (j === array.length) {
+      i++;
+      j = i + 1;
+    }
+  }
+
+  return res;
 }
+
+targetSum([10, -2, -7, 8, 5], 7);
+// [-2, 8]
